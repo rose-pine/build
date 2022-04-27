c18dfb1dbdba7aaafa97f08fe12efa654db968f5
mvllow <mvllow@icloud.com>
2022-04-27 19:18:12 -0400

    feat: add builtin variables

--- test/main.ts
+++ test/main.ts
@@ -1,25 +1,74 @@
+import fs from 'node:fs'
 import process from 'node:process'
 import test from 'ava'
 import build, {formatColor} from '../source/index.js'
 
 const mockDir = process.cwd() + '/test/mock'
 
-test('default', (t) => {
-	t.notThrows(async () => {
-		await build({
-			template: mockDir + '/template.json',
-			output: mockDir + '/dist',
-		})
+const readFile = (file: string, ext: string) =>
+	fs.readFileSync(`${mockDir}/dist/${file}.${ext}`, 'utf8')
+
+test('variable builtins', async (t) => {
+	await build({
+		template: mockDir + '/template.json',
+		output: mockDir + '/dist',
 	})
+
+	const [main, moon, dawn] = ['', '-moon', '-dawn'].map(
+		(v) =>
+			JSON.parse(readFile(`rose-pine${v}`, 'json')) as Record<string, string>
+	)
+
+	if (typeof main !== 'undefined') {
+		t.is(main['id'], 'rose-pine')
+		t.is(main['name'], 'Rosé Pine')
+		t.is(
+			main['description'],
+			'All natural pine, faux fur and a bit of soho vibes for the classy minimalist'
+		)
+		t.is(main['type'], 'dark')
+		t.is(main['foreground'], '#e0def4')
+		t.is(main['background'], '#191724')
+	}
+
+	if (typeof moon !== 'undefined') {
+		t.is(moon['id'], 'rose-pine-moon')
+		t.is(moon['name'], 'Rosé Pine Moon')
+		t.is(
+			moon['description'],
+			'All natural pine, faux fur and a bit of soho vibes for the classy minimalist'
+		)
+		t.is(moon['type'], 'dark')
+		t.is(moon['foreground'], '#e0def4')
+		t.is(moon['background'], '#232136')
+	}
+
+	if (typeof dawn !== 'undefined') {
+		t.is(dawn['id'], 'rose-pine-dawn')
+		t.is(dawn['name'], 'Rosé Pine Dawn')
+		t.is(
+			dawn['description'],
+			'All natural pine, faux fur and a bit of soho vibes for the classy minimalist'
+		)
+		t.is(dawn['type'], 'light')
+		t.is(dawn['foreground'], '#575279')
+		t.is(dawn['background'], '#faf4ed')
+	}
 })
 
-test('.txt template', async (t) => {
-	t.notThrows(async () => {
-		await build({
-			template: mockDir + '/template.txt',
-			output: mockDir + '/dist',
-		})
+test('txt template', async (t) => {
+	await build({
+		template: mockDir + '/template.txt',
+		output: mockDir + '/dist',
 	})
+
+	const [main, moon, dawn] = ['', '-moon', '-dawn'].map((v) =>
+		readFile(`rose-pine${v}`, 'txt').trim()
+	)
+
+	t.is(main, 'Rosé Pine can also be pronounced #ebbcba #31748f')
+	t.is(moon, 'Rosé Pine Moon can also be pronounced #ea9a97 #3e8fb0')
+	t.is(dawn, 'Rosé Pine Dawn can also be pronounced #d7827e #286983')
 })
 
 test('all formats', (t) => {
