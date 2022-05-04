c1743bd45f28a16722387599610b9c6461dbd12b
mvllow <mvllow@icloud.com>
2022-05-03 21:58:39 -0400

    test: every possible variable & format

--- test/format-color.ts
+++ test/format-color.ts
@@ -1,7 +1,7 @@
 import test from 'ava'
 import {formatColor} from '../source/utils/format-color.js'
 
-test('format color', (t) => {
+test('color formats', (t) => {
 	const testColor = {
 		hex: '#ebbcba',
 		rgb: 'rgb(235, 188, 186)',
@@ -28,4 +28,33 @@ test('format color', (t) => {
 	t.is(formatColor(testColor, 'hsl-ns', true), '255%83%')
 	t.is(formatColor(testColor, 'hsl-array', true), '[2,55%,83%]')
 	t.is(formatColor(testColor, 'hsl-function', true), 'hsl(2,55%,83%)')
+})
+
+test('alpha color formats', (t) => {
+	const testColor = {
+		hex: '#6e6a8614',
+		rgb: 'rgba(110, 106, 134, 0.08)',
+		hsl: 'hsla(249, 12%, 47%, 0.08)',
+	}
+
+	t.is(formatColor(testColor, 'hex'), '#6e6a8614')
+	t.is(formatColor(testColor, 'hex-ns'), '6e6a8614')
+	t.is(formatColor(testColor, 'hex', true), '#6e6a8614')
+	t.is(formatColor(testColor, 'hex-ns', true), '6e6a8614')
+	t.is(formatColor(testColor, 'rgb'), '110, 106, 134, 0.08')
+	t.is(formatColor(testColor, 'rgb-ns'), '110 106 134 0.08')
+	t.is(formatColor(testColor, 'rgb-array'), '[110, 106, 134, 0.08]')
+	t.is(formatColor(testColor, 'rgb-function'), 'rgba(110, 106, 134, 0.08)')
+	t.is(formatColor(testColor, 'rgb', true), '110,106,134,0.08')
+	t.is(formatColor(testColor, 'rgb-ns', true), '1101061340.08')
+	t.is(formatColor(testColor, 'rgb-array', true), '[110,106,134,0.08]')
+	t.is(formatColor(testColor, 'rgb-function', true), 'rgba(110,106,134,0.08)')
+	t.is(formatColor(testColor, 'hsl'), '249, 12%, 47%, 0.08')
+	t.is(formatColor(testColor, 'hsl-ns'), '249 12% 47% 0.08')
+	t.is(formatColor(testColor, 'hsl-array'), '[249, 12%, 47%, 0.08]')
+	t.is(formatColor(testColor, 'hsl-function'), 'hsla(249, 12%, 47%, 0.08)')
+	t.is(formatColor(testColor, 'hsl', true), '249,12%,47%,0.08')
+	t.is(formatColor(testColor, 'hsl-ns', true), '24912%47%0.08')
+	t.is(formatColor(testColor, 'hsl-array', true), '[249,12%,47%,0.08]')
+	t.is(formatColor(testColor, 'hsl-function', true), 'hsla(249,12%,47%,0.08)')
 })
--- test/main.ts
+++ test/main.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs'
 import process from 'node:process'
+import {variants} from '@rose-pine/palette'
 import test from 'ava'
 import build from '../source/index.js'
 
@@ -8,7 +9,7 @@ const mockDir = process.cwd() + '/test/mock'
 const readFile = (file: string, ext: string) =>
 	fs.readFileSync(`${mockDir}/dist/${file}.${ext}`, 'utf8')
 
-test('json template with default format', async (t) => {
+test('json template with hex values', async (t) => {
 	await build({
 		__skipReadmeVersion: true,
 		template: mockDir + '/template.json',
@@ -26,8 +27,23 @@ test('json template with default format', async (t) => {
 		description:
 			'All natural pine, faux fur and a bit of soho vibes for the classy minimalist',
 		colors: {
-			foreground: '#e0def4',
-			background: '#191724',
+			base: variants.main.base.hex,
+			surface: variants.main.surface.hex,
+			overlay: variants.main.overlay.hex,
+			muted: variants.main.muted.hex,
+			subtle: variants.main.subtle.hex,
+			love: variants.main.love.hex,
+			gold: variants.main.gold.hex,
+			rose: variants.main.rose.hex,
+			pine: variants.main.pine.hex,
+			foam: variants.main.foam.hex,
+			iris: variants.main.iris.hex,
+			highlightLow: variants.main.highlightLow.hex,
+			highlightLowAlpha: variants.main.highlightLow.alpha.hex,
+			highlightMed: variants.main.highlightMed.hex,
+			highlightMedAlpha: variants.main.highlightMed.alpha.hex,
+			highlightHigh: variants.main.highlightHigh.hex,
+			highlightHighAlpha: variants.main.highlightHigh.alpha.hex,
 		},
 	})
 
@@ -37,8 +53,23 @@ test('json template with default format', async (t) => {
 		description:
 			'All natural pine, faux fur and a bit of soho vibes for the classy minimalist',
 		colors: {
-			foreground: '#e0def4',
-			background: '#232136',
+			base: variants.moon.base.hex,
+			surface: variants.moon.surface.hex,
+			overlay: variants.moon.overlay.hex,
+			muted: variants.moon.muted.hex,
+			subtle: variants.moon.subtle.hex,
+			love: variants.moon.love.hex,
+			gold: variants.moon.gold.hex,
+			rose: variants.moon.rose.hex,
+			pine: variants.moon.pine.hex,
+			foam: variants.moon.foam.hex,
+			iris: variants.moon.iris.hex,
+			highlightLow: variants.moon.highlightLow.hex,
+			highlightLowAlpha: variants.moon.highlightLow.alpha.hex,
+			highlightMed: variants.moon.highlightMed.hex,
+			highlightMedAlpha: variants.moon.highlightMed.alpha.hex,
+			highlightHigh: variants.moon.highlightHigh.hex,
+			highlightHighAlpha: variants.moon.highlightHigh.alpha.hex,
 		},
 	})
 
@@ -48,8 +79,23 @@ test('json template with default format', async (t) => {
 		description:
 			'All natural pine, faux fur and a bit of soho vibes for the classy minimalist',
 		colors: {
-			foreground: '#575279',
-			background: '#faf4ed',
+			base: variants.dawn.base.hex,
+			surface: variants.dawn.surface.hex,
+			overlay: variants.dawn.overlay.hex,
+			muted: variants.dawn.muted.hex,
+			subtle: variants.dawn.subtle.hex,
+			love: variants.dawn.love.hex,
+			gold: variants.dawn.gold.hex,
+			rose: variants.dawn.rose.hex,
+			pine: variants.dawn.pine.hex,
+			foam: variants.dawn.foam.hex,
+			iris: variants.dawn.iris.hex,
+			highlightLow: variants.dawn.highlightLow.hex,
+			highlightLowAlpha: variants.dawn.highlightLow.alpha.hex,
+			highlightMed: variants.dawn.highlightMed.hex,
+			highlightMedAlpha: variants.dawn.highlightMed.alpha.hex,
+			highlightHigh: variants.dawn.highlightHigh.hex,
+			highlightHighAlpha: variants.dawn.highlightHigh.alpha.hex,
 		},
 	})
 })
@@ -69,21 +115,4 @@ test('txt template with hex values', async (t) => {
 	t.is(main, 'Rosé Pine can also be pronounced #ebbcba #31748f')
 	t.is(moon, 'Rosé Pine Moon can also be pronounced #ea9a97 #3e8fb0')
 	t.is(dawn, 'Rosé Pine Dawn can also be pronounced #d7827e #286983')
-})
-
-test('yml template with rgb values', async (t) => {
-	await build({
-		__skipReadmeVersion: true,
-		template: mockDir + '/template.txt',
-		output: mockDir + '/dist',
-		format: 'hex',
-	})
-
-	const [main, moon, dawn] = ['', '-moon', '-dawn'].map((v) =>
-		readFile(`rose-pine${v}`, 'yml').trim()
-	)
-
-	t.is(main, "active: 'rgb()' # or rgba()")
-	t.is(moon, "active: 'rgb()' # or rgba()")
-	t.is(dawn, "active: 'rgb()' # or rgba()")
 })
