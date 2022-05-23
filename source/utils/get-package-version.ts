2a2cccb7bd7d8f932321a85ba884ed3d5d6e5ae1
mvllow <mvllow@icloud.com>
2022-05-22 21:15:04 -0400

    chore: remove unused eslint disable

--- source/utils/get-package-version.ts
+++ source/utils/get-package-version.ts
@@ -2,7 +2,6 @@ import fs from 'node:fs'
 
 export const getPackageVersion = () => {
 	const {version = 'latest'} = JSON.parse(
-		// eslint-disable-next-line unicorn/prefer-json-parse-buffer
 		fs.readFileSync(new URL('../../package.json', import.meta.url), 'utf8')
 	) as Record<string, string>
 
