b9c69236f860e50d0abe55fc03558e7316a6c5ad
Joris Mertz <info@jorismertz.nl>
2024-05-05 20:42:42 +0200

    feat: support for template directories
    0.9.0
    
    fix: any 😡

--- test/main.js
+++ test/main.js
@@ -125,3 +125,39 @@ test("txt template with variant-unique values", async (t) => {
 	t.is(moon, "Rosé Pine Moon is our not as dark variant");
 	t.is(dawn, "Rosé Pine Dawn is our light variant");
 });
+
+test("template directory with multiple files", async (t) => {
+	await build({
+		__skipReadmeVersion: true,
+		template: mockDir + "/template",
+		output: mockDir + "/dist",
+	});
+
+	["main", "moon", "dawn"].forEach((variant) => {
+		const [json, txt] = ["json", "txt"].map((ext) =>
+			readFile(`${variant}/template.${ext}`).trim(),
+		);
+
+		const capitalizedVariant =
+			variant.charAt(0).toUpperCase() + variant.slice(1);
+
+		t.like(JSON.parse(json), {
+			id: `rose-pine${variant !== "main" ? `-${variant}` : ""}`,
+			name: "Rosé Pine" + (variant !== "main" ? ` ${capitalizedVariant}` : ""),
+			description:
+				"All natural pine, faux fur and a bit of soho vibes for the classy minimalist",
+		});
+
+		switch (variant) {
+			case "main":
+				t.is(txt, "Rosé Pine is our dark variant");
+				break;
+			case "moon":
+				t.is(txt, "Rosé Pine Moon is our not as dark variant");
+				break;
+			case "dawn":
+				t.is(txt, "Rosé Pine Dawn is our light variant");
+				break;
+		}
+	});
+});
